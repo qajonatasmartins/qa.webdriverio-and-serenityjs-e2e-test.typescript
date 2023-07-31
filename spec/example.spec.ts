e8a00ea1e2b9395c6bd8cf42590ca7aa0a9d02f2
Jonatas Faria @QAJONATASMARTINS <jonatas1503@hotmail.com>
2023-07-31 12:45:50 -0300

    Initial commit

---
+++ spec/example.spec.ts
@@ -0,0 +1,14 @@
+import { Ensure, includes } from '@serenity-js/assertions';
+import { actorCalled } from '@serenity-js/core';
+import { Navigate, Page } from '@serenity-js/web';
+import { describe, it } from 'mocha';
+
+describe('serenity-js.org website', () => {
+
+    it(`tells people what they're reading`, () =>
+        actorCalled('Alice').attemptsTo(
+            Navigate.to('https://serenity-js.org'),
+            
+            Ensure.that(Page.current().title(), includes('Serenity/JS')),
+        ));
+});
